5cb54984ffecdd19a9148d9b96d7df06509412ba
Claudius Graebner <claudius@claudius-graebner.com>
2020-04-15 11:42:58 +0200

    Add comparative statics; death rate reactivity still missing

--- app.R
+++ app.R
@@ -84,20 +84,47 @@ server <- function(input, output) {
   production <- function(total_factor_productivity, land, population, alpha_value){
     total_factor_productivity * land**alpha_value * population**(1-alpha_value)
   }
+  orig_color <- "#61a27c"
+  alt_color <- "#b8103b"
   
   pop_plot <- reactive({
-    birth_rate <- input$birth_rate
-    death_rate <- function(consumption){input$death_rate-log(consumption)}
-    g_c <- function(consumption, birthrate){birthrate - death_rate(consumption)}
-    G_c <- function(consumption, birthrate){1 + g_c(consumption, birthrate)}
-    
-    ggplot(data.frame(x=c(0,5)), aes(x)) + 
-      stat_function(fun=G_c, args = list(birthrate = birth_rate)) + 
-      ggtitle(TeX("Die Bevölkerungswachstumsfunktion $G(c_t)$")) +
-      xlab("Konsum") + ylab(TeX("$N_{t+1}/N_t$")) +
-      scale_y_continuous(expand = expansion()) +
-      theme_bw() + theme(panel.border = element_blank(), 
-                         axis.line = element_line())
+    if (input$compa_cases){
+      birth_rate <- input$birth_rate
+      birth_rate_2 <- input$birth_rate_2
+      death_rate <- function(consumption){input$death_rate-log(consumption)}
+      death_rate_2 <- function(consumption){input$death_rate_2-log(consumption)}
+      g_c <- function(consumption, birthrate){birthrate - death_rate(consumption)}
+      g_c_2 <- function(consumption, birthrate){birth_rate_2 - death_rate_2(consumption)}
+      G_c <- function(consumption, birthrate){1 + g_c(consumption, birthrate)}
+      G_c_2 <- function(consumption, birthrate){1 + g_c_2(consumption, birthrate)}
+      
+      ggplot(data.frame(x=c(0,5)), aes(x)) + 
+        stat_function(fun=G_c, args = list(birthrate = birth_rate), 
+                      aes(color="alt"), key_glyph = "rect") +
+        stat_function(fun=G_c_2, args = list(birthrate = birth_rate_2), 
+                      aes(color="neu"), key_glyph = "rect") +
+        scale_color_manual(values = c(orig_color, alt_color)) +
+        ggtitle(TeX("Die Bevölkerungswachstumsfunktion $G(c_t)$")) +
+        xlab("Konsum") + ylab(TeX("$N_{t+1}/N_t$")) +
+        scale_y_continuous(expand = expansion()) +
+        theme_bw() + theme(panel.border = element_blank(), 
+                           axis.line = element_line(), 
+                           legend.title = element_blank())
+    } else{
+      birth_rate <- input$birth_rate
+      death_rate <- function(consumption){input$death_rate-log(consumption)}
+      g_c <- function(consumption, birthrate){birthrate - death_rate(consumption)}
+      G_c <- function(consumption, birthrate){1 + g_c(consumption, birthrate)}
+      
+      ggplot(data.frame(x=c(0,5)), aes(x)) + 
+        stat_function(fun=G_c, args = list(birthrate = birth_rate)) + 
+        ggtitle(TeX("Die Bevölkerungswachstumsfunktion $G(c_t)$")) +
+        xlab("Konsum") + ylab(TeX("$N_{t+1}/N_t$")) +
+        scale_y_continuous(expand = expansion()) +
+        theme_bw() + theme(panel.border = element_blank(), 
+                           axis.line = element_line())
+    }
+
   })
   
   output$pop_plot <- renderPlot({
@@ -105,98 +132,261 @@ server <- function(input, output) {
   })
   
   prod_plot <- reactive({
-    L_0 <- input$L_0
-    N_0 <- input$N_0
-    tfp <- input$tfp
-    alpha_used <- input$alpha_used
+    if (input$compa_cases){
+      L_0 <- input$L_0
+      L_2 <- input$L_2
+      N_0 <- input$N_0
+      N_2 <- input$N_2
+      tfp <- input$tfp
+      tfp_2 <- input$tfp_2
+      alpha_used <- input$alpha_used
+      alpha_used_2 <- input$alpha_used_2
+      
+      ggplot(data.frame(x=c(0,2)), aes(x)) + 
+        stat_function(fun=production, 
+                      args = list(total_factor_productivity = tfp, 
+                                  land=L_0, alpha_value=alpha_used),
+                      aes(color="alt"), key_glyph = "rect") + 
+        stat_function(fun=production, 
+                      args = list(total_factor_productivity = tfp_2, 
+                                  land=L_2, alpha_value=alpha_used_2),
+                      aes(color="neu"), key_glyph = "rect") + 
+        scale_color_manual(values = c(orig_color, alt_color)) +
+        ggtitle("Die Produktionsfunktion") +
+        scale_y_continuous(expand = expansion()) +
+        xlab("Bevölkerung") + ylab(TeX("$Y_t$")) +
+        theme_bw() + theme(panel.border = element_blank(), 
+                           axis.line = element_line(), 
+                           legend.title = element_blank())
+    } else{
+      L_0 <- input$L_0
+      N_0 <- input$N_0
+      tfp <- input$tfp
+      alpha_used <- input$alpha_used
+      
+      ggplot(data.frame(x=c(0,2)), aes(x)) + 
+        stat_function(fun=production, args = list(total_factor_productivity = tfp, 
+                                                  land=L_0, alpha_value=alpha_used)) + 
+        ggtitle("Die Produktionsfunktion") +
+        scale_y_continuous(expand = expansion()) +
+        xlab("Bevölkerung") + ylab(TeX("$Y_t$")) +
+        theme_bw() + theme(panel.border = element_blank(), 
+                           axis.line = element_line())
+    }
 
-    ggplot(data.frame(x=c(0,2)), aes(x)) + 
-      stat_function(fun=production, args = list(total_factor_productivity = tfp, 
-                                                land=L_0, alpha_value=alpha_used)) + 
-      ggtitle("Die Produktionsfunktion") +
-      scale_y_continuous(expand = expansion()) +
-      xlab("Bevölkerung") + ylab(TeX("$Y_t$")) +
-      theme_bw() + theme(panel.border = element_blank(), 
-                         axis.line = element_line())
   })
   output$prod_plot <- renderPlot({
     prod_plot()
   })
-  
+
   consumption_plot <- reactive({
-    birth_rate <- input$birth_rate
-    death_rate <- function(consumption){input$death_rate-log(consumption)}
-    g_c <- function(consumption, birthrate){birthrate - death_rate(consumption)}
-    G_c <- function(consumption, birthrate){1 + g_c(consumption, birthrate)}
-    
-    get_c_ss <- function(birth_rate){
-      g_c_1 <- function(consumption, birthrate){G_c(consumption, birthrate) - 1}
-      uniroot(g_c_1, interval = c(0, 10), extendInt = "yes", birthrate=birth_rate)$root
+    if (input$compa_cases){
+      birth_rate <- input$birth_rate
+      birth_rate_2 <- input$birth_rate_2
+      death_rate <- function(consumption){input$death_rate-log(consumption)}
+      death_rate_2 <- function(consumption){input$death_rate_2-log(consumption)}
+      g_c <- function(consumption, birthrate){birthrate - death_rate(consumption)}
+      g_c_2 <- function(consumption, birthrate){birth_rate_2 - death_rate_2(consumption)}
+      G_c <- function(consumption, birthrate){1 + g_c(consumption, birthrate)}
+      G_c_2 <- function(consumption, birthrate){1 + g_c_2(consumption, birthrate)}
+      
+      get_c_ss <- function(birth_rate){
+        g_c_1 <- function(consumption, birthrate){G_c(consumption, birthrate) - 1}
+        uniroot(g_c_1, interval = c(0, 10), extendInt = "yes", birthrate=birth_rate)$root
+      }
+      
+      c_ss <- get_c_ss(birth_rate)
+      c_ss_new <- get_c_ss(birth_rate_2)
+      
+      ggplot(data.frame(x=c(0,5)), aes(x)) + 
+        stat_function(fun=G_c, args = list(birthrate = birth_rate),
+                      aes(color="alt"), key_glyph = "rect") + 
+        stat_function(fun=G_c_2, args = list(birthrate = birth_rate_2),
+                      aes(color="neu"), key_glyph = "rect") + 
+        scale_color_manual(values = c(orig_color, alt_color)) +
+        ggtitle("Gleichgewichtskonsum") +
+        scale_y_continuous(expand = expansion()) +
+        xlab("Konsum") + ylab(TeX("$N_{t+1}/N_t$")) +
+        geom_segment(aes(x = -Inf, y = 1, xend = c_ss, yend = 1), 
+                     linetype="dashed", color=orig_color) +
+        geom_segment(aes(x = c_ss, y = -Inf, xend = c_ss, yend = 1), 
+                     linetype="dashed", color=orig_color) +
+        geom_segment(aes(x = -Inf, y = 1, xend = c_ss_new, yend = 1), 
+                     linetype="dashed", color=alt_color) +
+        geom_segment(aes(x = c_ss_new, y = -Inf, xend = c_ss_new, yend = 1), 
+                     linetype="dashed", color=alt_color) +
+        theme_bw() + theme(panel.border = element_blank(), 
+                           axis.line = element_line(), 
+                           legend.title = element_blank())
+    } else{
+      birth_rate <- input$birth_rate
+      death_rate <- function(consumption){input$death_rate-log(consumption)}
+      g_c <- function(consumption, birthrate){birthrate - death_rate(consumption)}
+      G_c <- function(consumption, birthrate){1 + g_c(consumption, birthrate)}
+      
+      get_c_ss <- function(birth_rate){
+        g_c_1 <- function(consumption, birthrate){G_c(consumption, birthrate) - 1}
+        uniroot(g_c_1, interval = c(0, 10), extendInt = "yes", birthrate=birth_rate)$root
+      }
+      
+      c_ss <- get_c_ss(birth_rate)
+      
+      ggplot(data.frame(x=c(0,5)), aes(x)) + 
+        stat_function(fun=G_c, args = list(birthrate = birth_rate)) + 
+        ggtitle("Gleichgewichtskonsum") +
+        scale_y_continuous(expand = expansion()) +
+        xlab("Konsum") + ylab(TeX("$N_{t+1}/N_t$")) +
+        geom_segment(aes(x = -Inf, y = 1, xend = c_ss, yend = 1), linetype="dashed") +
+        geom_segment(aes(x = c_ss, y = -Inf, xend = c_ss, yend = 1), linetype="dashed") +
+        theme_bw() + theme(panel.border = element_blank(), axis.line = element_line())
     }
-    
-    c_ss <- get_c_ss(birth_rate)
-    
-    ggplot(data.frame(x=c(0,5)), aes(x)) + 
-      stat_function(fun=G_c, args = list(birthrate = birth_rate)) + 
-      ggtitle("Gleichgewichtskonsum") +
-      scale_y_continuous(expand = expansion()) +
-      xlab("Konsum") + ylab(TeX("$N_{t+1}/N_t$")) +
-      geom_segment(aes(x = -Inf, y = 1, xend = c_ss, yend = 1), linetype="dashed") +
-      geom_segment(aes(x = c_ss, y = -Inf, xend = c_ss, yend = 1), linetype="dashed") +
-      theme_bw() + theme(panel.border = element_blank(), axis.line = element_line())
+
   })
   output$consumption_plot <- renderPlot({
     consumption_plot()
   })
   population_plot <- reactive({
-    L_0 <- input$L_0
-    N_0 <- input$N_0
-    tfp <- input$tfp
-    alpha_used <- input$alpha_used
-    birth_rate <- input$birth_rate
-    
-    death_rate <- function(consumption){input$death_rate-log(consumption)}
-    g_c <- function(consumption, birthrate){birthrate - death_rate(consumption)}
-    G_c <- function(consumption, birthrate){1 + g_c(consumption, birthrate)}
-    
-    get_c_ss <- function(birth_rate){
-      g_c_1 <- function(consumption, birthrate){G_c(consumption, birthrate) - 1}
-      uniroot(g_c_1, interval = c(0, 10), extendInt = "yes", birthrate=birth_rate)$root
-    }
-    
-    c_ss <- get_c_ss(birth_rate)
-    
-    interval_check <- seq(0, 2, 0.05)
-    
-    total_consumption <- function(pop_total, consumption_total){
-      pop_total*consumption_total
-    }
-    
-    n_ss <- uniroot(function(x) total_consumption(
-      x, consumption_total=c_ss)-production(
-        population=x, 
+    if (input$compa_cases){
+      L_0 <- input$L_0
+      L_2 <- input$L_2
+      N_0 <- input$N_0
+      N_2 <- input$N_2
+      tfp <- input$tfp
+      tfp_2 <- input$tfp_2
+      alpha_used <- input$alpha_used
+      alpha_used_2 <- input$alpha_used_2
+      
+      birth_rate <- input$birth_rate
+      birth_rate_2 <- input$birth_rate_2
+      death_rate <- function(consumption){input$death_rate-log(consumption)}
+      death_rate_2 <- function(consumption){input$death_rate_2-log(consumption)}
+      g_c <- function(consumption, birthrate){birthrate - death_rate(consumption)}
+      g_c_2 <- function(consumption, birthrate){birth_rate_2 - death_rate_2(consumption)}
+      G_c <- function(consumption, birthrate){1 + g_c(consumption, birthrate)}
+      G_c_2 <- function(consumption, birthrate){1 + g_c_2(consumption, birthrate)}
+      
+      get_c_ss <- function(birth_rate){
+        g_c_1 <- function(consumption, birthrate){G_c(consumption, birthrate) - 1}
+        uniroot(g_c_1, interval = c(0, 10), extendInt = "yes", birthrate=birth_rate)$root
+      }
+      
+      c_ss <- get_c_ss(birth_rate)
+      c_ss_new <- get_c_ss(birth_rate_2)
+      
+      interval_check <- seq(0, 2, 0.05)
+      
+      total_consumption <- function(pop_total, consumption_total){
+        pop_total*consumption_total
+      }
+      
+      n_ss <- uniroot(function(x) total_consumption(
+        x, consumption_total=c_ss)-production(
+          population=x, 
+          total_factor_productivity=tfp, 
+          land=L_0, 
+          alpha_value = alpha_used), 
+        c(0.5, 2.5), extendInt = "yes")$root
+      
+      n_ss_new <- uniroot(function(x) total_consumption(
+        x, consumption_total=c_ss_new)-production(
+          population=x, 
+          total_factor_productivity=tfp_2, 
+          land=L_2, 
+          alpha_value = alpha_used_2), 
+        c(0.5, 2.5), extendInt = "yes")$root
+      
+      y_ss <- production(
+        population=n_ss, 
         total_factor_productivity=tfp, 
         land=L_0, 
-        alpha_value = alpha_used), 
-      c(0.5, 2.5), extendInt = "yes")$root
-    
-    y_ss <- production(
-      population=n_ss, 
-      total_factor_productivity=tfp, 
-      land=L_0, 
-      alpha_value = alpha_used)
-    
-    ggplot(data.frame(x=c(0, 2)), aes(x)) + 
-      stat_function(fun=production, args = list(total_factor_productivity = tfp, 
-                                                land=L_0, alpha_value=alpha_used)) + 
-      ggtitle("Das Bevölkerungsgleichgewicht") +
-      scale_y_continuous(expand = expansion()) +
-      xlab("Bevölkerung") + ylab(TeX("$Y_t$")) +
-      geom_segment(aes(x = -Inf, y = y_ss, xend = n_ss, yend = y_ss), linetype="dashed") +
-      geom_segment(aes(x = n_ss, y = -Inf, xend = n_ss, yend = y_ss), linetype="dashed") +
-      geom_abline(intercept = 0, slope = c_ss) +
-      theme_bw() + theme(panel.border = element_blank(), 
-                         axis.line = element_line())
+        alpha_value = alpha_used)
+      
+      y_ss_new <- production(
+        population=n_ss_new, 
+        total_factor_productivity=tfp_2, 
+        land=L_2, 
+        alpha_value = alpha_used_2)
+      
+      ggplot(data.frame(x=c(0, 2)), aes(x)) + 
+        stat_function(fun=production, 
+                      args = list(total_factor_productivity = tfp, 
+                                  land=L_0, alpha_value=alpha_used),
+                      aes(color="alt"), key_glyph = "rect"
+                      ) + 
+        stat_function(fun=production, 
+                      args = list(total_factor_productivity = tfp_2, 
+                                  land=L_2, alpha_value=alpha_used_2),
+                      aes(color="neu"), key_glyph = "rect"
+        ) + 
+        geom_abline(intercept = 0, slope = c_ss, 
+                    color=orig_color, key_glyph = "rect") +
+        geom_abline(intercept = 0, slope = c_ss_new, 
+                    color=alt_color, key_glyph = "rect") +
+        scale_color_manual(values = c(orig_color, alt_color)) +
+        ggtitle("Das Bevölkerungsgleichgewicht") +
+        scale_y_continuous(expand = expansion()) +
+        xlab("Bevölkerung") + ylab(TeX("$Y_t$")) +
+        geom_segment(aes(x = -Inf, y = y_ss, xend = n_ss, yend = y_ss), 
+                     linetype="dashed", color=orig_color) +
+        geom_segment(aes(x = n_ss, y = -Inf, xend = n_ss, yend = y_ss), 
+                     linetype="dashed", color=orig_color) +
+        geom_segment(aes(x = -Inf, y = y_ss_new, xend = n_ss_new, yend = y_ss_new), 
+                     linetype="dashed", color=alt_color) +
+        geom_segment(aes(x = n_ss_new, y = -Inf, xend = n_ss_new, yend = y_ss_new), 
+                     linetype="dashed", color=alt_color) +
+        theme_bw() + theme(panel.border = element_blank(), 
+                           axis.line = element_line(), 
+                           legend.title = element_blank())
+    } else{
+      L_0 <- input$L_0
+      N_0 <- input$N_0
+      tfp <- input$tfp
+      alpha_used <- input$alpha_used
+      birth_rate <- input$birth_rate
+      
+      death_rate <- function(consumption){input$death_rate-log(consumption)}
+      g_c <- function(consumption, birthrate){birthrate - death_rate(consumption)}
+      G_c <- function(consumption, birthrate){1 + g_c(consumption, birthrate)}
+      
+      get_c_ss <- function(birth_rate){
+        g_c_1 <- function(consumption, birthrate){G_c(consumption, birthrate) - 1}
+        uniroot(g_c_1, interval = c(0, 10), extendInt = "yes", birthrate=birth_rate)$root
+      }
+      
+      c_ss <- get_c_ss(birth_rate)
+      
+      interval_check <- seq(0, 2, 0.05)
+      
+      total_consumption <- function(pop_total, consumption_total){
+        pop_total*consumption_total
+      }
+      
+      n_ss <- uniroot(function(x) total_consumption(
+        x, consumption_total=c_ss)-production(
+          population=x, 
+          total_factor_productivity=tfp, 
+          land=L_0, 
+          alpha_value = alpha_used), 
+        c(0.5, 2.5), extendInt = "yes")$root
+      
+      y_ss <- production(
+        population=n_ss, 
+        total_factor_productivity=tfp, 
+        land=L_0, 
+        alpha_value = alpha_used)
+      
+      ggplot(data.frame(x=c(0, 2)), aes(x)) + 
+        stat_function(fun=production, args = list(total_factor_productivity = tfp, 
+                                                  land=L_0, alpha_value=alpha_used)) + 
+        ggtitle("Das Bevölkerungsgleichgewicht") +
+        scale_y_continuous(expand = expansion()) +
+        xlab("Bevölkerung") + ylab(TeX("$Y_t$")) +
+        geom_segment(aes(x = -Inf, y = y_ss, xend = n_ss, yend = y_ss), linetype="dashed") +
+        geom_segment(aes(x = n_ss, y = -Inf, xend = n_ss, yend = y_ss), linetype="dashed") +
+        geom_abline(intercept = 0, slope = c_ss) +
+        theme_bw() + theme(panel.border = element_blank(), 
+                           axis.line = element_line())
+    }
   })
   output$population_plot <- renderPlot({
     population_plot()
