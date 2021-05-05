61841eea626c6e92270282d58b55a1f3c0f57090
Claudius Graebner <claudius.graebner@posteo.de>
2021-05-05 12:30:54 +0200

    Update Versionstest to allow for newer R versions

--- versionstest_malthus.R
+++ versionstest_malthus.R
@@ -4,20 +4,19 @@
 # melden Sie sich bitte mit einem Screenshot der Fehlermeldung bei Claudius Gräbner
 
 # Ich empfehle Ihnen zudem Ihre R Version zu updaten wenn Sie nicht mindestens
-# Version 3.6.1 verwenden. Dieses Sktipt überprüft auch die aktuelle Version von
+# Version 4.0.1 verwenden. Dieses Sktipt überprüft auch die aktuelle Version von
 # R und gibt Ihnen einen Hinweis wenn Sie updaten sollten.
 
 versionstest <- c(
-  as.double(R.version$major)<3,
-  as.double(substr(R.version$minor, 1, 1))<6,
-  as.double(substr(R.version$minor, 1, 1))<6
+  as.double(R.version$major)<4,
+  as.double(substr(R.version$minor, 3, 3))<1
 )
 
 if (TRUE %in% versionstest){
   print(paste0(
     "ACHTUNG! Ihre R-Version ist veraltet! ",
     "Sie verwenden Version ", R.version$major, ".", R.version$minor, 
-    ", aber Sie sollten mindestens Version 3.6.1 verwenden. ",
+    ", aber Sie sollten mindestens Version 4.0.1 verwenden. ",
     "Installieren Sie zum Update R einfach neu!")
   )
 } else{
@@ -27,11 +26,20 @@ if (TRUE %in% versionstest){
 install.packages(c('devtools', 'shiny', 'ggplot2', 'dplyr', 
                    'latex2exp', 'magrittr', 'ggpubr'))
 
+if (!require(devtools)) install.packages('devtools')
+if (!require(shiny)) install.packages('shiny')
+if (!require(tidyverse)) install.packages('tidyverse')
+if (!require(magrittr)) install.packages('latex2exp')
+if (!require(magrittr)) install.packages('magrittr')
+if (!require(ggpubr)) install.packages('ggpubr')
+
 
 ggplot_version <- as.character(packageVersion("ggplot2"))
 
-if ((as.double(substr(ggplot_version, 1, 1)) == 3) &  
-    (as.double(substr(ggplot_version, 3, 3)) >= 4) ) {
+ggplot_version <- as.character(packageVersion("ggplot2"))
+
+if ((as.double(substr(ggplot_version, 1, 1)) >= 3) &  
+    (as.double(substr(ggplot_version, 3, 3)) >= 3) ) {
   print("ggplot2 ist ausreichend aktuell.")
 } else {
   cat(paste0(
